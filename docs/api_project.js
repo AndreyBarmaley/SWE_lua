527aea55bffb7563281c0131e44b670766ec7fb8
Andrey Af <public.irkutsk@gmail.com>
2021-02-20 16:21:50 +0800

    update docs, ver 20210220

--- docs/api_project.js
+++ docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-02-18T16:33:34.650Z",
+    "time": "2021-02-20T07:56:29.929Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
